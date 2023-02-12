0b1c800e269f4bc0681410b1481a7ba780d825d0
BunaIas <123762465+BunaIas@users.noreply.github.com>
2023-02-12 14:32:24 +0200

    Update index.js

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -1,1 +1,31 @@
 
+function onMessage(t){
+            const e = t.data.slice(8);
+             console.log(e);
+        }
+const WebSocket = require('ws');
+const webSocket = new WebSocket('wss://mt5wademo.fftrader.cz/terminal');
+webSocket.binaryType = 'arraybuffer';
+
+webSocket.on('open', () => {
+  console.log('WebSocket connection established');
+});
+
+webSocket.on('message', onMessage);
+
+webSocket.on('error', (error) => {
+  console.error('WebSocket connection error:', error);
+});
+
+webSocket.on('close', (event) => {
+  console.log('WebSocket connection closed with code:', event.code, 'reason:', event.reason);
+});
+webSocket.on('close', (event) => {
+  console.log('WebSocket connection closed:', event);
+});
+setTimeout(() => {
+  if (webSocket.readyState !== WebSocket.OPEN) {
+    console.error('WebSocket connection could not be established');
+    webSocket.close();
+  }
+}, 5000);
