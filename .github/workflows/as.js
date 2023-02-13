8bebef8497f1fcd89eeae0eae8d24fe36b3dd77a
BunaIas <123762465+BunaIas@users.noreply.github.com>
2023-02-14 11:02:26 +1400

    Update as.js

--- .github/workflows/as.js
+++ .github/workflows/as.js
@@ -14,13 +14,7 @@ const puppeteer = require('puppeteer');
             "height": 657
         })
     }
-    {
-        const targetPage = page;
-        const promises = [];
-        promises.push(targetPage.waitForNavigation());
-        await targetPage.goto("chrome://new-tab-page/");
-        await Promise.all(promises);
-    }
+   
     {
         const targetPage = page;
         const promises = [];
@@ -28,6 +22,24 @@ const puppeteer = require('puppeteer');
         await targetPage.goto("https://mt5wademo.fftrader.cz/terminal");
         await Promise.all(promises);
     }
+    {
+        
+        const targetPage = page;
+        await scrollIntoViewIfNeeded([["aria/Accept", "aria/[role=\"generic\"]"], ["body > div > div > div.window.svelte-1927787.draggable > div > div.body.svelte-1927787 > div > div.button.svelte-yxtt66 > button > div"], ["xpath//html/body/div/div/div[2]/div/div[2]/div/div[3]/button/div"], ["text/Accept"]], targetPage, timeout);
+        const element = await waitForSelectors([["aria/Accept", "aria/[role=\"generic\"]"], ["body > div > div > div.window.svelte-1927787.draggable > div > div.body.svelte-1927787 > div > div.button.svelte-yxtt66 > button > div"], ["xpath//html/body/div/div/div[2]/div/div[2]/div/div[3]/button/div"], ["text/Accept"]], targetPage, {
+            timeout,
+            visible: true
+        });
+        await element.click({
+            offset: {
+                x: 14.6875,
+                y: 4,
+            },
+        });
+    
+    }  
+    
+    
     {
         const targetPage = page;
         await scrollIntoViewIfNeeded([["aria/Enter Login"], ["body > div > div > div.window.svelte-1927787.draggable > div > div.body.svelte-1927787 > div > div.content.svelte-1hrkl1v > form > span > input"], ["xpath//html/body/div/div/div[2]/div/div[2]/div/div[3]/form/span/input"]], targetPage, timeout);
