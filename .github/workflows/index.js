b0fba95597997608f3446d54093c1f453c526f20
BunaIas <123762465+BunaIas@users.noreply.github.com>
2023-02-16 13:03:35 +1400

    Update index.js

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -252,16 +252,11 @@ const puppeteer = require('puppeteer');
 async function myFunction() {
   console.log("This function was executed after 5 seconds");
 const targetPage = page;
-  const selector = 'body > div > div.left-panel.svelte-1du3hrq > div > div.wrap.svelte-1du3hrq > div > div.content.svelte-1y3frqs > div.footer-row.svelte-o8v493 > button:nth-child(2)';
-  const element = await page.$(selector);
-
-  if (element) {
-    await element.click();
-  } else {
-    console.log(`Element with selector ${selector} not found`);
-  }
-}
-
+  
+await page.evaluate(() => {
+  const button = document.querySelector('aria/Buy by Market'); // replace 'button' with the selector for your button
+  button.click();
+});
 setTimeout(myFunction, 5000);
 
  
