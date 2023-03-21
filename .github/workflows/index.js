982a21c9bf4114dce5c1b7cb90fbee276d3f35c6
BunaIas <123762465+BunaIas@users.noreply.github.com>
2023-03-22 12:01:51 +1400

    Update index.js

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -118,20 +118,21 @@ const puppeteer = require('puppeteer');
             },
         });
     }
-    {
-        const targetPage = page;
-        await scrollIntoViewIfNeeded([["body > div > div.right-panel.svelte-moa9ns > div > label > input"], ["xpath//html/body/div/div[4]/div/label/input"]], targetPage, timeout);
-        const element = await waitForSelectors([["body > div > div.right-panel.svelte-moa9ns > div > label > input"], ["xpath//html/body/div/div[4]/div/label/input"]], targetPage, {
-            timeout,
-            visible: true
-        });
-        await element.click({
-            offset: {
-                x: 56.89581298828125,
-                y: 14.059024810791016,
-            },
-        });
-    }
+   {
+    const targetPage = page;
+    await scrollIntoViewIfNeeded([["body > div.layout.svelte-ezbv2f > div.right-panel.svelte-moa9ns > div > label > input"], ["xpath//html/body/div[2]/div[4]/div/label/input"]], targetPage, timeout);
+    const element = await waitForSelectors([["body > div.layout.svelte-ezbv2f > div.right-panel.svelte-moa9ns > div > label > input"], ["xpath//html/body/div[2]/div[4]/div/label/input"]], targetPage, {
+        timeout,
+        visible: true
+    });
+    await element.click({
+        offset: {
+            x: 40.1180419921875,
+            y: 18.059024810791016,
+        },
+    });
+}
+
     {
         const targetPage = page;
         await scrollIntoViewIfNeeded([["body > div > div.right-panel.svelte-moa9ns > div > label > input"], ["xpath//html/body/div/div[4]/div/label/input"]], targetPage, timeout);
