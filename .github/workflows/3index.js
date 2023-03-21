81cb4a3c4edc3a17a36a6b4059dd811f7546b604
BunaIas <123762465+BunaIas@users.noreply.github.com>
2023-03-22 11:25:52 +1400

    Create 3index.js

---
+++ .github/workflows/3index.js
@@ -0,0 +1,332 @@
+const puppeteer = require('puppeteer');
+// v13.0.0 or later
+
+(async()=>{
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const timeout = 5000;
+    page.setDefaultTimeout(timeout);
+
+    {
+        const targetPage = page;
+        await targetPage.setViewport({
+            "width": 768,
+            "height": 694
+        })
+    }
+    {
+        const targetPage = page;
+        const promises = [];
+        promises.push(targetPage.waitForNavigation());
+        await targetPage.goto("https://mt5wademo.fftrader.cz/terminal");
+        await Promise.all(promises);
+    }
+    {
+        const targetPage = page;
+        await scrollIntoViewIfNeeded([["aria/Accept", "aria/[role=\"generic\"]"], ["body > div._portal > div > div.window.svelte-evog0x.draggable > div > div.body.svelte-evog0x > div > div.button.svelte-1wr2l3c > button > div"], ["xpath//html/body/div[2]/div/div[2]/div/div[2]/div/div[3]/button/div"], ["text/Accept"]], targetPage, timeout);
+        const element = await waitForSelectors([["aria/Accept", "aria/[role=\"generic\"]"], ["body > div._portal > div > div.window.svelte-evog0x.draggable > div > div.body.svelte-evog0x > div > div.button.svelte-1wr2l3c > button > div"], ["xpath//html/body/div[2]/div/div[2]/div/div[2]/div/div[3]/button/div"], ["text/Accept"]], targetPage, {
+            timeout,
+            visible: true
+        });
+        await element.click({
+            offset: {
+                x: 59.30206298828125,
+                y: 4.0867919921875,
+            },
+        });
+    }
+    {
+        const targetPage = page;
+        await scrollIntoViewIfNeeded([["aria/Enter Login[role=\"textbox\"]"], ["body > div._portal > div > div.window.svelte-evog0x.draggable > div > div.body.svelte-evog0x > div > div.login-content.svelte-1oicbsl > div.content.svelte-1oicbsl > div > div > form > span > input"], ["xpath//html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div[2]/div/div/form/span/input"]], targetPage, timeout);
+        const element = await waitForSelectors([["aria/Enter Login[role=\"textbox\"]"], ["body > div._portal > div > div.window.svelte-evog0x.draggable > div > div.body.svelte-evog0x > div > div.login-content.svelte-1oicbsl > div.content.svelte-1oicbsl > div > div > form > span > input"], ["xpath//html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div[2]/div/div/form/span/input"]], targetPage, {
+            timeout,
+            visible: true
+        });
+        await element.click({
+            offset: {
+                x: 41.038177490234375,
+                y: 16.576385498046875,
+            },
+        });
+    }
+    {
+        const targetPage = page;
+        await scrollIntoViewIfNeeded([["aria/Enter Login[role=\"textbox\"]"], ["body > div._portal > div > div.window.svelte-evog0x.draggable > div > div.body.svelte-evog0x > div > div.login-content.svelte-1oicbsl > div.content.svelte-1oicbsl > div > div > form > span > input"], ["xpath//html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div[2]/div/div/form/span/input"]], targetPage, timeout);
+        const element = await waitForSelectors([["aria/Enter Login[role=\"textbox\"]"], ["body > div._portal > div > div.window.svelte-evog0x.draggable > div > div.body.svelte-evog0x > div > div.login-content.svelte-1oicbsl > div.content.svelte-1oicbsl > div > div > form > span > input"], ["xpath//html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div[2]/div/div/form/span/input"]], targetPage, {
+            timeout,
+            visible: true
+        });
+        const inputType = await element.evaluate(el=>el.type);
+        if (inputType === 'select-one') {
+            await changeSelectElement(element, "1051543824")
+        } else if (["textarea", "text", "url", "tel", "search", "password", "number", "email"].includes(inputType)) {
+            await typeIntoElement(element, "1051543824");
+        } else {
+            await changeElementValue(element, "1051543824");
+        }
+    }
+    {
+        const targetPage = page;
+        await scrollIntoViewIfNeeded([["aria/Enter Password[role=\"textbox\"]"], ["body > div._portal > div > div.window.svelte-evog0x.draggable > div > div.body.svelte-evog0x > div > div.login-content.svelte-1oicbsl > div.content.svelte-1oicbsl > div > div > form > div.layout.svelte-1bpvxjz > div:nth-child(1) > div > span > input"], ["xpath//html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div[2]/div/div/form/div[4]/div[1]/div/span/input"]], targetPage, timeout);
+        const element = await waitForSelectors([["aria/Enter Password[role=\"textbox\"]"], ["body > div._portal > div > div.window.svelte-evog0x.draggable > div > div.body.svelte-evog0x > div > div.login-content.svelte-1oicbsl > div.content.svelte-1oicbsl > div > div > form > div.layout.svelte-1bpvxjz > div:nth-child(1) > div > span > input"], ["xpath//html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div[2]/div/div/form/div[4]/div[1]/div/span/input"]], targetPage, {
+            timeout,
+            visible: true
+        });
+        await element.click({
+            offset: {
+                x: 29.038177490234375,
+                y: 14.3680419921875,
+            },
+        });
+    }
+    {
+        const targetPage = page;
+        await scrollIntoViewIfNeeded([["aria/Enter Password[role=\"textbox\"]"], ["body > div._portal > div > div.window.svelte-evog0x.draggable > div > div.body.svelte-evog0x > div > div.login-content.svelte-1oicbsl > div.content.svelte-1oicbsl > div > div > form > div.layout.svelte-1bpvxjz > div:nth-child(1) > div > span > input"], ["xpath//html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div[2]/div/div/form/div[4]/div[1]/div/span/input"]], targetPage, timeout);
+        const element = await waitForSelectors([["aria/Enter Password[role=\"textbox\"]"], ["body > div._portal > div > div.window.svelte-evog0x.draggable > div > div.body.svelte-evog0x > div > div.login-content.svelte-1oicbsl > div.content.svelte-1oicbsl > div > div > form > div.layout.svelte-1bpvxjz > div:nth-child(1) > div > span > input"], ["xpath//html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div[2]/div/div/form/div[4]/div[1]/div/span/input"]], targetPage, {
+            timeout,
+            visible: true
+        });
+        const inputType = await element.evaluate(el=>el.type);
+        if (inputType === 'select-one') {
+            await changeSelectElement(element, "1WF3KPK9RU")
+        } else if (["textarea", "text", "url", "tel", "search", "password", "number", "email"].includes(inputType)) {
+            await typeIntoElement(element, "1WF3KPK9RU");
+        } else {
+            await changeElementValue(element, "1WF3KPK9RU");
+        }
+    }
+    {
+        const targetPage = page;
+        await scrollIntoViewIfNeeded([["aria/Connect to account[role=\"button\"]"], ["body > div._portal > div > div.window.svelte-evog0x.draggable > div > div.body.svelte-evog0x > div > div.login-content.svelte-1oicbsl > div.content.svelte-1oicbsl > div > div > form > div.footer.svelte-1yec9gl > div > button"], ["xpath//html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div[2]/div/div/form/div[7]/div/button"]], targetPage, timeout);
+        const element = await waitForSelectors([["aria/Connect to account[role=\"button\"]"], ["body > div._portal > div > div.window.svelte-evog0x.draggable > div > div.body.svelte-evog0x > div > div.login-content.svelte-1oicbsl > div.content.svelte-1oicbsl > div > div > form > div.footer.svelte-1yec9gl > div > button"], ["xpath//html/body/div[2]/div/div[2]/div/div[2]/div/div[1]/div[2]/div/div/form/div[7]/div/button"]], targetPage, {
+            timeout,
+            visible: true
+        });
+        await element.click({
+            offset: {
+                x: 52.3541259765625,
+                y: 21.20135498046875,
+            },
+        });
+    }
+  /*
+  {
+    const targetPage = page;
+    await scrollIntoViewIfNeeded([["body > div.layout.svelte-ezbv2f > div.right-panel.svelte-moa9ns > div > label > div.icon.left.svelte-1nc9ygh > div > div > svg"], ["xpath//html/body/div[2]/div[5]/div/label/div[1]/div/div/svg"]], targetPage, timeout);
+    const element = await waitForSelectors([["body > div.layout.svelte-ezbv2f > div.right-panel.svelte-moa9ns > div > label > div.icon.left.svelte-1nc9ygh > div > div > svg"], ["xpath//html/body/div[2]/div[5]/div/label/div[1]/div/div/svg"]], targetPage, {
+        timeout,
+        visible: true
+    });
+    await element.click({
+        offset: {
+            x: 4.12152099609375,
+            y: 12.065967559814453,
+        },
+    });
+}
+*/
+    await browser.close();
+
+    async function waitForSelectors(selectors, frame, options) {
+        for (const selector of selectors) {
+            try {
+                return await waitForSelector(selector, frame, options);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        throw new Error('Could not find element for selectors: ' + JSON.stringify(selectors));
+    }
+
+    async function scrollIntoViewIfNeeded(selectors, frame, timeout) {
+        const element = await waitForSelectors(selectors, frame, {
+            visible: false,
+            timeout
+        });
+        if (!element) {
+            throw new Error('The element could not be found.');
+        }
+        await waitForConnected(element, timeout);
+        const isInViewport = await element.isIntersectingViewport({
+            threshold: 0
+        });
+        if (isInViewport) {
+            return;
+        }
+        await element.evaluate(element=>{
+            element.scrollIntoView({
+                block: 'center',
+                inline: 'center',
+                behavior: 'auto',
+            });
+        }
+        );
+        await waitForInViewport(element, timeout);
+    }
+
+    async function waitForConnected(element, timeout) {
+        await waitForFunction(async()=>{
+            return await element.getProperty('isConnected');
+        }
+        , timeout);
+    }
+
+    async function waitForInViewport(element, timeout) {
+        await waitForFunction(async()=>{
+            return await element.isIntersectingViewport({
+                threshold: 0
+            });
+        }
+        , timeout);
+    }
+
+    async function waitForSelector(selector, frame, options) {
+        if (!Array.isArray(selector)) {
+            selector = [selector];
+        }
+        if (!selector.length) {
+            throw new Error('Empty selector provided to waitForSelector');
+        }
+        let element = null;
+        for (let i = 0; i < selector.length; i++) {
+            const part = selector[i];
+            if (element) {
+                element = await element.waitForSelector(part, options);
+            } else {
+                element = await frame.waitForSelector(part, options);
+            }
+            if (!element) {
+                throw new Error('Could not find element: ' + selector.join('>>'));
+            }
+            if (i < selector.length - 1) {
+                element = (await element.evaluateHandle(el=>el.shadowRoot ? el.shadowRoot : el)).asElement();
+            }
+        }
+        if (!element) {
+            throw new Error('Could not find element: ' + selector.join('|'));
+        }
+        return element;
+    }
+
+    async function waitForElement(step, frame, timeout) {
+        const count = step.count || 1;
+        const operator = step.operator || '>=';
+        const comp = {
+            '==': (a,b)=>a === b,
+            '>=': (a,b)=>a >= b,
+            '<=': (a,b)=>a <= b,
+        };
+        const compFn = comp[operator];
+        await waitForFunction(async()=>{
+            const elements = await querySelectorsAll(step.selectors, frame);
+            return compFn(elements.length, count);
+        }
+        , timeout);
+    }
+
+    async function querySelectorsAll(selectors, frame) {
+        for (const selector of selectors) {
+            const result = await querySelectorAll(selector, frame);
+            if (result.length) {
+                return result;
+            }
+        }
+        return [];
+    }
+
+    async function querySelectorAll(selector, frame) {
+        if (!Array.isArray(selector)) {
+            selector = [selector];
+        }
+        if (!selector.length) {
+            throw new Error('Empty selector provided to querySelectorAll');
+        }
+        let elements = [];
+        for (let i = 0; i < selector.length; i++) {
+            const part = selector[i];
+            if (i === 0) {
+                elements = await frame.$$(part);
+            } else {
+                const tmpElements = elements;
+                elements = [];
+                for (const el of tmpElements) {
+                    elements.push(...(await el.$$(part)));
+                }
+            }
+            if (elements.length === 0) {
+                return [];
+            }
+            if (i < selector.length - 1) {
+                const tmpElements = [];
+                for (const el of elements) {
+                    const newEl = (await el.evaluateHandle(el=>el.shadowRoot ? el.shadowRoot : el)).asElement();
+                    if (newEl) {
+                        tmpElements.push(newEl);
+                    }
+                }
+                elements = tmpElements;
+            }
+        }
+        return elements;
+    }
+
+    async function waitForFunction(fn, timeout) {
+        let isActive = true;
+        const timeoutId = setTimeout(()=>{
+            isActive = false;
+        }
+        , timeout);
+        while (isActive) {
+            const result = await fn();
+            if (result) {
+                clearTimeout(timeoutId);
+                return;
+            }
+            await new Promise(resolve=>setTimeout(resolve, 100));
+        }
+        throw new Error('Timed out');
+    }
+
+    async function changeSelectElement(element, value) {
+        await element.select(value);
+        await element.evaluateHandle((e)=>{
+            e.blur();
+            e.focus();
+        }
+        );
+    }
+
+    async function changeElementValue(element, value) {
+        await element.focus();
+        await element.evaluate((input,value)=>{
+            input.value = value;
+            input.dispatchEvent(new Event('input',{
+                bubbles: true
+            }));
+            input.dispatchEvent(new Event('change',{
+                bubbles: true
+            }));
+        }
+        , value);
+    }
+
+    async function typeIntoElement(element, value) {
+        const textToType = await element.evaluate((input,newValue)=>{
+            if (newValue.length <= input.value.length || !newValue.startsWith(input.value)) {
+                input.value = '';
+                return newValue;
+            }
+            const originalValue = input.value;
+            input.value = '';
+            input.value = originalValue;
+            return newValue.substring(originalValue.length);
+        }
+        , value);
+        await element.type(textToType);
+    }
+}
+)().catch(err=>{
+    console.error(err);
+    process.exit(1);
+}
+);
