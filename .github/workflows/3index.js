b6e172a0d216852195ff5bc9c1dfeca214b6f1fc
BunaIas <123762465+BunaIas@users.noreply.github.com>
2023-07-07 09:09:15 +1400

    Update 3index.js

--- .github/workflows/3index.js
+++ .github/workflows/3index.js
@@ -544,10 +544,10 @@ black_hole = new Date('2023-04-28T21:42:00.000Z');
 //drink = [];
 //console.log(forecast)
 
-console.log(new Date(new Date().getTime() + 3 * 60 * 60 * 1000));
+//console.log(new Date(new Date().getTime() + 3 * 60 * 60 * 1000));
     
 while ( new Date(new Date().getTime() + 10800000) < gravity ){
-//  console.log(new Date(new Date().getTime() + 10800000))
+  console.log(new Date(new Date().getTime() + 10800000))
     
 let actual = await Promise.all([
 ...drink.map(k => page2.evaluate((k) => document.querySelectorAll('span#actual')[k].textContent,k)),
