d41940064ce97aafeebd3c1d27f1e455235fc5d1
BunaIas <123762465+BunaIas@users.noreply.github.com>
2023-08-12 22:36:35 +1400

    Update 2index.js

--- .github/workflows/2index.js
+++ .github/workflows/2index.js
@@ -8,7 +8,6 @@ const puppeteer = require('puppeteer');
 (async()=>{
     const browser = await puppeteer.launch({  headless: 'new' , slowMo: 5, });
 
-    
 let singularity = '2023-08-04 | 12:00 | 12:01 | 3  12:00 x ETH x Industrial Production (MoM) (Jun) x 479390';
 
 
