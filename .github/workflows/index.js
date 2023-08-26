850d099aec7d37f5d9392fb87571ded5d557b6b7
BunaIas <123762465+BunaIas@users.noreply.github.com>
2023-08-27 09:22:58 +1400

    Update index.js

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -5,6 +5,18 @@ console.log(`My constant is: ${g.substring(0, 3)}`+"liot");
 const puppeteer = require('puppeteer');
 // v13.0.0 or later
 
+let singularity = "2023-08-26 | 22:10 | 11:01 | 3  normal x EUR x 11:00 x HCOB Manufacturing PMI Flash JUL x 43.5 x 43.4 nor eco |, normal x EUR x 11:00 x HCOB Composite PMI Flash JUL x 49.7 x 49.9 nor eco |, normal x EUR x 11:00 x HCOB Services PMI Flash JUL x 51.5 x 52 nor eco |"
+
+
+let drink = [326403,0];
+let breath = [480148, 1111];
+let actual = [];
+
+let hour_array = singularity.slice(13,18);
+let gravity = new Date(singularity.slice(0,10)+'T'+hour_array+':07.000Z');
+let black_hole = new Date(singularity.slice(0,10)+'T'+hour_array+':59.000Z')
+
+
 (async()=>{
     const browser = await puppeteer.launch({ headless: 'new', });
     const page = await browser.newPage();
@@ -31,9 +43,13 @@ const puppeteer = require('puppeteer');
    
     const html = await page.content();
 
-  console.log(html); // Display the HTML content
+const until = setInterval(async () => {
+    if(new Date(new Date().getTime() + 10800000) > gravity){process.exit()
+
+    }
+  }, 1);
    
-    await browser.close();
+
 
     async function waitForSelectors(selectors, frame, options) {
         for (const selector of selectors) {
