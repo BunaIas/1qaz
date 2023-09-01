abf4e335914472940a85424fdbc4bee634c46250
BunaIas <123762465+BunaIas@users.noreply.github.com>
2023-09-01 21:57:53 +1400

    Update index.js

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -9,7 +9,7 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({  headless: 'new', });
 
     
-let singularity = '2023-09-01 | 10:55 | 15:31 | 3  10:30 x EUR x Industrial Production (MoM) (Jun) x 39.1 x 326766 x 480609 x normal ||'
+let singularity = '2023-09-01 | 11:30 | 15:31 | 3  10:30 x GBP x Industrial Production (MoM) (Jun) x 42.5 x 326766 x 480617 x normal ||'
 
 
     
