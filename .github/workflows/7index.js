91933e040c5e3c02ed4672b2fb41ab2df93b280c
BunaIas <123762465+BunaIas@users.noreply.github.com>
2023-09-05 21:43:42 +1400

    Update 7index.js

--- .github/workflows/7index.js
+++ .github/workflows/7index.js
@@ -8,7 +8,6 @@ const puppeteer = require('puppeteer');
 (async()=>{
     const browser = await puppeteer.launch({  headless: 'new', });
 
-    
 let singularity = '2023-09-05 | 11:50 | 15:31 | 3  10:30 x EUR x Industrial Production (MoM) (Jun) x 0 x 326974 x 480763 x normal ||'
 
 
