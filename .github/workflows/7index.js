850be25406263d82cda961799e085890ad0a2592
BunaIas <123762465+BunaIas@users.noreply.github.com>
2023-09-06 17:20:14 +0300

    Update 7index.js

--- .github/workflows/7index.js
+++ .github/workflows/7index.js
@@ -41,7 +41,7 @@ let hour_array = singularity.slice(13,18);
 
   
  const page = await browser.newPage();
-    const timeout = 10000;
+    const timeout = 20000;
     page.setDefaultTimeout(timeout);
 
     {
