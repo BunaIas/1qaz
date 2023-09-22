2123c11f3f07ad455169c7b2a4814957b50afe71
BunaIas <123762465+BunaIas@users.noreply.github.com>
2023-09-22 23:59:51 +0300

    Update 2index.js

--- .github/workflows/2index.js
+++ .github/workflows/2index.js
@@ -9,11 +9,9 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({  headless: 'new' , slowMo: 10, });
 
     
-let singularity = '2023-09-22 | 12:21 | 17:01 | 3   x normal x USD x 12:11 x Existing Home Sales (Aug) x 327789 new eco x 481859 nor inv x 4.1 ||,  x normal x USD x 17:00 x US Leading Index (MoM) (Aug) x 327906 new eco x 481861 nor inv x -0.5 ||';
+//let singularity = '2023-09-22 | 12:21 | 17:01 | 3   x normal x USD x 12:11 x Existing Home Sales (Aug) x 327789 new eco x 481859 nor inv x 4.1 ||,  x normal x USD x 17:00 x US Leading Index (MoM) (Aug) x 327906 new eco x 481861 nor inv x -0.5 ||';
 
 
-
-  
 let come_down = singularity.slice(32);
 let achilles = come_down.split(' x ');
 let steel = achilles[2];
