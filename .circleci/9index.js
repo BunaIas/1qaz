15a4a5a803227b55462a967032418a4a6a42733b
BunaIas <123762465+BunaIas@users.noreply.github.com>
2023-10-04 11:07:24 +0300

    Update 9index.js

--- .circleci/9index.js
+++ .circleci/9index.js
@@ -10,7 +10,7 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({  headless: 'new', });
     
 
-let singularity = '2023-10-04 | 11:06 | 11:31 | 3   x normal x ILS x 12:11 x Existing Home Sales (Aug) x 327789 new eco x 481859 nor inv x 4.1 ||,  x normal x USD x 17:00 x US Leading Index (MoM) (Aug) x 327906 new eco x 481861 nor inv x -0.5 ||';
+//let singularity = '2023-10-04 | 11:06 | 11:31 | 3   x normal x ILS x 12:11 x Existing Home Sales (Aug) x 327789 new eco x 481859 nor inv x 4.1 ||,  x normal x USD x 17:00 x US Leading Index (MoM) (Aug) x 327906 new eco x 481861 nor inv x -0.5 ||';
 
 
   
